c530bb9cfd578412654d3ea5752b9c293aad4451
Alin Eugen Deac <ade@vestergaardcompany.com>
2016-08-09 08:09:46 +0200

    Add timeout exception

---
+++ src/packet/Exceptions/TimeoutException.php
@@ -0,0 +1,15 @@
+<?php
+namespace oliverlorenz\reactphpmqtt\packet\Exceptions;
+
+use RuntimeException;
+
+/**
+ * Timeout Exception
+ *
+ * @author Alin Eugen Deac <ade@vestergaardcompany.com>
+ * @package oliverlorenz\reactphpmqtt\packet\Exceptions
+ */
+class TimeoutException extends RuntimeException
+{
+
+}
