2fa1363bbd355ce2064441ebadd58d7ac8c962f0
Alin Eugen Deac <ade@vestergaardcompany.com>
2016-08-09 08:11:51 +0200

    Add base exception
    Good for situations when you have multiple components
    and need to deal with exception handling for those
    various components.

---
+++ src/packet/Exceptions/PhpMqttClientException.php
@@ -0,0 +1,17 @@
+<?php
+namespace oliverlorenz\reactphpmqtt\packet\Exceptions;
+
+use RuntimeException;
+
+/**
+ * Php Mqtt Client Exception
+ *
+ * Base exception for all Mqtt Client exceptions.
+ *
+ * @author Alin Eugen Deac <ade@vestergaardcompany.com>
+ * @package oliverlorenz\reactphpmqtt\packet\Exceptions
+ */
+class PhpMqttClientException extends RuntimeException
+{
+
+}
