50fd5a59adbfe7dee9eb131fe4f3073cf7c5e4e9
Alin Eugen Deac <ade@vestergaardcompany.com>
2016-08-09 08:16:43 +0200

    Add stream exception

---
+++ src/packet/Exceptions/StreamException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace oliverlorenz\reactphpmqtt\packet\Exceptions;
+
+/**
+ * Stream Exception
+ *
+ * @author Alin Eugen Deac <ade@vestergaardcompany.com>
+ * @package oliverlorenz\reactphpmqtt\packet\Exceptions
+ */
+class StreamException extends PhpMqttClientException
+{
+
+}
